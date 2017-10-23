a2872b5660b89af5137a02be7169ae6c788b31b7
Kim, Hirokuni <yangkookkim@gmail.com>
2017-10-23 16:12:03 +0900

    Updating Slate to 2.0.0

--- config.rb
+++ config.rb
@@ -1,3 +1,6 @@
+# Unique header generation
+require './lib/unique_head.rb'
+
 # Markdown
 set :markdown_engine, :redcarpet
 set :markdown,
@@ -7,7 +10,8 @@ set :markdown,
     prettify: true,
     tables: true,
     with_toc_data: true,
-    no_intra_emphasis: true
+    no_intra_emphasis: true,
+    renderer: UniqueHeadCounter
 
 # Assets
 set :css_dir, 'stylesheets'
@@ -17,6 +21,17 @@ set :fonts_dir, 'fonts'
 
 # Activate the syntax highlighter
 activate :syntax
+ready do
+  require './lib/multilang.rb'
+end
+
+activate :sprockets
+
+activate :autoprefixer do |config|
+  config.browsers = ['last 2 version', 'Firefox ESR']
+  config.cascade  = false
+  config.inline   = true
+end
 
 # Github pages require relative links
 activate :relative_assets
@@ -24,9 +39,19 @@ set :relative_links, true
 
 # Build Configuration
 configure :build do
+  # If you're having trouble with Middleman hanging, commenting
+  # out the following two lines has been known to help
   activate :minify_css
   activate :minify_javascript
   # activate :relative_assets
   # activate :asset_hash
   # activate :gzip
+end
+
+# Deploy Configuration
+# If you want Middleman to listen on a different port, you can set that below
+set :port, 4567
+
+helpers do
+  require './lib/toc_data.rb'
 end
